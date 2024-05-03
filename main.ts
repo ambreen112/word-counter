982ada00781bba93d58a74ae217607c277c35eaa
unknown <ambreenjahan144@gmail.com>
2024-05-03 12:06:26 +0500

    this is word counter

---
+++ main.ts
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+
+import inquerir from "inquirer";
+import chalk from "chalk";
+
+         console.log(chalk.bold.red("This Is A word Counter"));
+        
+
+const answ:{
+    sentence: string
+}
+   = await inquerir.prompt([{
+    name: "sentence",
+    type: "input",
+    message: (chalk.blue("Enter your sentence to count the word:"))
+}])
+
+const words = answ.sentence.trim().split(" ")
+
+console.log(words);
+
+console.log(chalk.red(`Your sentence word count is ${words.length}`));
+
